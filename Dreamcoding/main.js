0a212a58ca3a02b9d038fa6e1129affc1928c88c
Just_David <rjtp5670@gmail.com>
2020-12-23 23:55:09 +0900

    Navbar menu click

--- Dreamcoding/main.js
+++ Dreamcoding/main.js
@@ -15,3 +15,24 @@ document.addEventListener("scroll", () => {
     console.log("Not true");
   }
 });
+
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbaMenu = document.querySelector(".navbar__menu");
+navbaMenu.addEventListener("click", (event)=> {
+  console.log(event.target.dataset);
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return; // 링크가 존재하지 않을경우, 곧바로 리턴후 종료되도록함. 
+  }
+  console.log(link); // 링크가 존재 할경우 링크 출력. 
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+  
+  /* console.log("document.querySelector(link):" + scrollTo); */
+  /* console.log("Link:" + link); */
+  
+});
